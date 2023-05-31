5ab9e881f3fb56ae77939ffb00a374ba4816d56d
Chelsea Kwong <ckwong@whatnot.com>
2023-05-31 14:46:10 -0700

    remove pin state

--- app/src/main/kotlin/com/whatnot/sample/rx/ProductList.kt
+++ app/src/main/kotlin/com/whatnot/sample/rx/ProductList.kt
@@ -8,7 +8,5 @@ class ProductList(
   private val fetchProductList: FetchProductList,
   private val productEvents: ProductEvents,
 ) {
-  fun pinnedProduct(): Observable<Optional<AuctionProduct>> = TODO()
-
   fun asObservable(): Observable<List<AuctionProduct>> = TODO()
 }
